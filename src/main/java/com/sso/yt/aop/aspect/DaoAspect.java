734460fb7097f96bbb0404c62d718377d310b409
易涛 <909513070@qq.com>
2017-07-07 19:34:18 +0800

    工具类调整

--- src/main/java/com/sso/yt/aop/aspect/DaoAspect.java
+++ src/main/java/com/sso/yt/aop/aspect/DaoAspect.java
@@ -1,6 +1,8 @@
 package com.sso.yt.aop.aspect;
 
-import com.yt.mybatis.model.BaseModel;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
@@ -9,19 +11,45 @@ import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
+import com.yt.mybatis.model.BaseModel;
 
 /**
  * Created by yt on 2017-1-24.
+ * 统一给insert和update操作添加创建人和修改人，并且设置创建时间和修改时间为null，使用数据库自动生成的时间 Created by yt on 2017-1-22.
  */
 @Component
 @Aspect
-public class DalAspect {
+@SuppressWarnings({ "rawtypes", "unchecked" })
+public class DaoAspect {
+
+    private static ThreadLocal<String> loginName = new ThreadLocal<>();
     private static final String CREATOR = "creator";
     private static final String EDITOR = "editor";
 
     private static final String CREATE_TIME = "createTime";
-    private static final String EDIT_TIME = "editTime";
+    private static final String LAST_TIME = "last_time";
+
+    private static final String DEFAULT = "admin";
+
+    /**
+     * 设置aop程序需要使用的登陆用户名
+     * @param name
+     */
+    public static void setLoginName(String name) {
+        loginName.set(name);
+    }
+
+    /**
+     * 此方法禁止开放
+     * @return
+     */
+    private static String getLoginName() {
+        if (StringUtils.isNotEmpty(loginName.get()))
+            return loginName.get();
+        else
+            return DEFAULT;
+    }
+
 
     @Pointcut("execution(* com.yt.mybatis.model.BaseMapper.insert*(..))")
     public void insertPointcut() {
@@ -44,19 +72,19 @@ public class DalAspect {
         if (args[0] instanceof BaseModel) {
             BeanWrapper beanWrapper = new BeanWrapperImpl(args[0]);
             if (beanWrapper.isWritableProperty(EDITOR)) {
-                beanWrapper.setPropertyValue(EDITOR, "admin_editor");
+                beanWrapper.setPropertyValue(EDITOR, getLoginName());
             }
-            if (beanWrapper.isWritableProperty(EDIT_TIME)) {
-                beanWrapper.setPropertyValue(EDIT_TIME, null);
+            if (beanWrapper.isWritableProperty(LAST_TIME)) {
+                beanWrapper.setPropertyValue(LAST_TIME, null);
             }
         } else if (args[0] instanceof Map) {
             Map map = (Map) args[0];
             if (map.containsKey(EDITOR)) {
-                map.put(EDITOR, "admin_editor");
+                map.put(EDITOR, getLoginName());
             }
 
-            if (map.containsKey(EDIT_TIME)) {
-                map.put(EDIT_TIME, null);
+            if (map.containsKey(LAST_TIME)) {
+                map.put(LAST_TIME, null);
             }
         }
     }
@@ -70,18 +98,30 @@ public class DalAspect {
         if (args[0] instanceof BaseModel) {
             BeanWrapper beanWrapper = new BeanWrapperImpl(args[0]);
             if (beanWrapper.isWritableProperty(CREATOR)) {
-                beanWrapper.setPropertyValue(CREATOR, "admin");
+                beanWrapper.setPropertyValue(CREATOR, getLoginName());
             }
             if (beanWrapper.isWritableProperty(CREATE_TIME)) {
                 beanWrapper.setPropertyValue(CREATE_TIME, null);
             }
+            if (beanWrapper.isWritableProperty(EDITOR)) {
+                beanWrapper.setPropertyValue(EDITOR, getLoginName());
+            }
+            if (beanWrapper.isWritableProperty(LAST_TIME)) {
+                beanWrapper.setPropertyValue(LAST_TIME, null);
+            }
         } else if (args[0] instanceof Map) {
             Map map = (Map) args[0];
             if (map.containsKey(CREATOR)) {
-                map.put(CREATOR, "admin");
+                map.put(CREATOR, getLoginName());
             }
             if (map.containsKey(CREATE_TIME)) {
                 map.put(CREATE_TIME, null);
+            }
+            if (map.containsKey(EDITOR)) {
+                map.put(EDITOR, getLoginName());
+            }
+            if (map.containsKey(LAST_TIME)) {
+                map.put(LAST_TIME, null);
             }
         }
     }
