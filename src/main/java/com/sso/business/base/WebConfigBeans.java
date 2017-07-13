e17f3498a8fffec1da648ed678a49fad22cc5eb6
易涛 <909513070@qq.com>
2017-07-13 09:18:00 +0800

    用户验证登录完成

--- src/main/java/com/sso/business/base/WebConfigBeans.java
+++ src/main/java/com/sso/business/base/WebConfigBeans.java
@@ -1,41 +1,22 @@
 package com.sso.business.base;
 
-import javax.annotation.PostConstruct;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.support.GenericConversionService;
-import org.springframework.format.support.FormattingConversionServiceFactoryBean;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.support.ConfigurableWebBindingInitializer;
-import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
-
-import com.google.common.collect.Sets;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Created by yt on 2017-7-6.
  */
 @Configuration
-@Controller
-public class WebConfigBeans {
+public class WebConfigBeans extends WebMvcConfigurerAdapter {
 
-	@Autowired
-	private RequestMappingHandlerAdapter handlerAdapter;
 
 	/**
 	 * 增加字符串转日期的功能
 	 */
-
-	@PostConstruct
-	public void initEditableValidation() {
-
-
-		ConfigurableWebBindingInitializer initializer = (ConfigurableWebBindingInitializer) handlerAdapter.getWebBindingInitializer();
-		if (initializer.getConversionService() != null) {
-			GenericConversionService genericConversionService = (GenericConversionService) initializer.getConversionService();
-			genericConversionService.addConverter(new StringToDateConverter());
-		}
+	@Override
+	public void addFormatters(FormatterRegistry registry) {
+		super.addFormatters(registry);
+		registry.addConverter(new StringToDateConverter());
 	}
 }
